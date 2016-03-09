37e4b149aa3decf0f005b39620f22b5c28139548
Lauri Mäkinen <lauri.ae.makinen@helsinki.fi>
2016-03-09 23:17:33 +0200

    Etusivun queryn päivitystä ja tietokantamuutoksien päivittelyä koodiin

--- src/main/java/tikape/chat/Database.java
+++ src/main/java/tikape/chat/Database.java
@@ -78,7 +78,7 @@ public class Database {
 
         // heroku käyttää SERIAL-avainsanaa uuden tunnuksen automaattiseen luomiseen
         lista.add("CREATE TABLE Kayttaja(nimimerkki VARCHAR(50) PRIMARY KEY, salasana VARCHAR(20) NOT NULL, CONSTRAINT Tarkastus CHECK (LENGTH(nimimerkki) > 2 AND LENGTH(salasana > 2)));");
-        lista.add("CREATE TABLE Viesti(id SERIAL PRIMARY KEY, nimimerkki VARCHAR(50), keskustelu_id integer, teksti VARCHAR(160) NOT NULL, pvm TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (nimimerkki) REFERENCES Kayttaja (nimimerkki), FOREIGN KEY (keskustelu_id) REFERENCES Keskustelu (id));");
+        lista.add("CREATE TABLE Viesti(id SERIAL PRIMARY KEY, nimimerkki VARCHAR(50), keskustelu_id integer, alue_id integer,teksti VARCHAR(160) NOT NULL, pvm TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (nimimerkki) REFERENCES Kayttaja (nimimerkki), FOREIGN KEY (keskustelu_id) REFERENCES Keskustelu (id), FOREIGN KEY (alue_id) REFERENCES Alue (id) );");
         lista.add("CREATE TABLE Keskustelu(id SERIAL PRIMARY KEY, alue_id integer, otsikko text NOT NULL, FOREIGN KEY (alue_id) REFERENCES Alue (id), CONSTRAINT Tarkastus CHECK(LENGTH(otsikko) > 2));");
         lista.add("CREATE TABLE Alue(id SERIAL PRIMARY KEY, nimi VARCHAR(100) NOT NULL);");
 
@@ -97,12 +97,12 @@ public class Database {
         lista.add("INSERT INTO Keskustelu(alue_id, otsikko) VALUES(3, 'Tietokannat')");
         lista.add("INSERT INTO Keskustelu(alue_id, otsikko) VALUES(3, 'Chat-sovellus')");
 
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 1, 'Ei ole olemassa harmaata taloutta, kaikki on mustaa');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 2, 'Hampaampaa on');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 3, 'Kemistit on neroja!');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 4, 'Mitenkäs tähän voisi liittyä?');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 5, 'Tietokannat on kyl sekasin nyt');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 6, 'Tuleekohan vaan valmiiksi....');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 1, 1, 'Ei ole olemassa harmaata taloutta, kaikki on mustaa');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 2, 1, 'Hampaampaa on');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 3, 2, 'Kemistit on neroja!');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 4, 2, 'Mitenkäs tähän voisi liittyä?');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 5, 3, 'Tietokannat on kyl sekasin nyt');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 6, 3, 'Tuleekohan vaan valmiiksi....');");
         return lista;
     }
 
@@ -111,7 +111,7 @@ public class Database {
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
         lista.add("CREATE TABLE Kayttaja(nimimerkki VARCHAR(50) PRIMARY KEY, salasana VARCHAR(20) NOT NULL, CONSTRAINT Tarkastus CHECK (LENGTH(nimimerkki) > 2 AND LENGTH(salasana > 2)));");
-        lista.add("CREATE TABLE Viesti(id integer PRIMARY KEY, nimimerkki VARCHAR(50), keskustelu_id integer, teksti VARCHAR(160) NOT NULL, pvm TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (nimimerkki) REFERENCES Kayttaja (nimimerkki), FOREIGN KEY (keskustelu_id) REFERENCES Keskustelu (id));");
+        lista.add("CREATE TABLE Viesti(id integer PRIMARY KEY, nimimerkki VARCHAR(50), keskustelu_id integer, alue_id integer, teksti VARCHAR(160) NOT NULL, pvm TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, FOREIGN KEY (nimimerkki) REFERENCES Kayttaja (nimimerkki), FOREIGN KEY (keskustelu_id) REFERENCES Keskustelu (id), FOREIGN KEY (alue_id) REFERENCES Alue (id));");
         lista.add("CREATE TABLE Keskustelu(id integer PRIMARY KEY, alue_id integer, otsikko text NOT NULL, FOREIGN KEY (alue_id) REFERENCES Alue (id), CONSTRAINT Tarkastus CHECK(LENGTH(otsikko) > 2));");
         lista.add("CREATE TABLE Alue(id integer PRIMARY KEY, nimi VARCHAR(100) NOT NULL);");
 
@@ -130,12 +130,12 @@ public class Database {
         lista.add("INSERT INTO Keskustelu(alue_id, otsikko) VALUES(3, 'Tietokannat');");
         lista.add("INSERT INTO Keskustelu(alue_id, otsikko) VALUES(3, 'Chat-sovellus');");
 
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 1, 'Ei ole olemassa harmaata taloutta, kaikki on mustaa');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 2, 'Hampaampaa on');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 3, 'Kemistit on neroja!');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 4, 'Mitenkäs tähän voisi liittyä?');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 5, 'Tietokannat on kyl sekasin nyt');");
-        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 6, 'Tuleekohan vaan valmiiksi....');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 1, 1, 'Ei ole olemassa harmaata taloutta, kaikki on mustaa');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 2, 1, 'Hampaampaa on');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 3, 2, 'Kemistit on neroja!');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 4, 2, 'Mitenkäs tähän voisi liittyä?');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 5, 3, 'Tietokannat on kyl sekasin nyt');");
+        lista.add("INSERT INTO Viesti(nimimerkki, keskustelu_id, teksti) VALUES('Teme', 6, 3, 'Tuleekohan vaan valmiiksi....');");
 
         return lista;
     }
